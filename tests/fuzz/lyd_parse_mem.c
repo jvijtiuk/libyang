910633003f632419e42d5a9009fc076bdb5f2300
Juraj Vijtiuk <juraj.vijtiuk@sartura.hr>
2020-09-11 17:35:19 +0200

    test6

--- tests/fuzz/lyd_parse_mem.c
+++ tests/fuzz/lyd_parse_mem.c
@@ -74,7 +74,7 @@ int LLVMFuzzerTestOneInput(uint8_t const *buf, size_t len)
 	}
 	memcpy(data, buf, len);
 	data[len] = 0;
-	printf("test5\n");
+	printf("test6\n");
 
   struct lyd_node *tree = NULL;
 	lyd_parse_data_mem(ctx, data, LYD_XML, 0, LYD_VALIDATE_PRESENT, &tree);
